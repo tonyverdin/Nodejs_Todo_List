a9ab7ba1458dbb79af5b1b015e80e18d366b2f28
Tony <tony.verdin@gmail.com>
2020-02-04 15:18:11 -0600

    Original Push

---
+++ date.js
@@ -0,0 +1,12 @@
+//jshint esversion:6
+module.exports = getdate;
+function getdate(){
+let today = new Date();
+let options = {
+  weekday: "long",
+  day:"numeric",
+  month:"long"
+};
+let day = today.toLocaleDateString("en-US", options);
+return day;
+}
